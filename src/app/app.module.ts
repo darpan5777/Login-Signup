2fcad91a50d6666e2934e6d3182ed31227682b81
Darpan Parmar <85171559+darpan5777@users.noreply.github.com>
2021-09-24 10:43:25 +0530

    login-signup

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    SignupComponent
+   
+
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+    ,ReactiveFormsModule,
+    FormsModule,HttpClientModule
+    
+  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
